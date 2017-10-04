a82197f15d0aabfd3428bdbbc99a08f043c80c65
Damo <damonkhan92@gmail.com>
2017-10-04 22:16:11 +1300

    Initial commit

---
+++ src/com/damonkhan/Main.java
@@ -0,0 +1,11 @@
+package com.damonkhan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char myChar = '\u00A9'; // unicode for copyright symbol
+	System.out.println("Welcome to the House of Khan "+ myChar);
+	System.out.println("Please be seated and someone will be with you momentarily");
+
+    }
+}
